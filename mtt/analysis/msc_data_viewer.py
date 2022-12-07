0e24f9e1f2d9560514b47fc7e238ffc5998aa916
Qiangx7 <62750920+QZx7@users.noreply.github.com>
2022-12-07 15:04:54 +0900

    update base chat service

--- mtt/analysis/msc_data_viewer.py
+++ mtt/analysis/msc_data_viewer.py
@@ -11,27 +11,24 @@ def load_data(data_path: Text) -> List:
 
 def view_data(data_list: List):
     for item in data_list:
-        input_text = input("command\n")
-        if input_text == 'x':
-            print(item)
-            # display previous dialog
-            for previous_dialog in item["previous_dialogs"]:
-                print("===============================")
-                print(previous_dialog["time_back"])
-                for utt_index in range(len(previous_dialog["dialog"])):
-                    if utt_index % 2 == 0:
-                        print(previous_dialog["dialog"][utt_index]["text"])
-                    else:
-                        print("          " + previous_dialog["dialog"][utt_index]["text"])
-            # display current dialog
+        print("*************************************************************")
+        
+        print("*************************************************************")
+        for previous_dialog in item["previous_dialogs"]:
             print("===============================")
-            for utt_index in range(len(item["dialog"])):
+            print(previous_dialog["time_back"])
+            for utt_index in range(len(previous_dialog["dialog"])):
                 if utt_index % 2 == 0:
-                    print(item["dialog"][utt_index]["text"])
+                    print(previous_dialog["dialog"][utt_index]["text"])
                 else:
-                    print("          " + item["dialog"][utt_index]["text"])
-        else:
-            break
+                    print("          " + previous_dialog["dialog"][utt_index]["text"])
+        # display current dialog
+        print("===============================")
+        for utt_index in range(len(item["dialog"])):
+            if utt_index % 2 == 0:
+                print(item["dialog"][utt_index]["text"])
+            else:
+                print("          " + item["dialog"][utt_index]["text"])
 
 
 if __name__ == "__main__":
